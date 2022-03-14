460ff389906d1079f614f306d8696bf41df62a4b
Saif-El-Eslam <SAIF82820@GMAIL.COM>
2022-03-14 10:52:05 -0700

    .

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -16,10 +16,10 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
         <Spinner />
       ) : (
         <Fragment>
-          <h1 className="large text-primary">Developers</h1>
+          { /*<h1 className="large text-primary"></h1> */}
           <p className="lead">
-            <i className="fab fa-connectdevelop" /> Browse and connect with
-            developers
+            <i className="fab fa-connectdevelop" /> connect with
+            job seekers
           </p>
           <div className="profiles">
             {profiles.length > 0 ? (
