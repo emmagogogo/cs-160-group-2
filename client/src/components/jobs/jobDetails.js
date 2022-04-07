e7e6633f472ca6cf1d3d3e53344a6c1a899d5ab2
Emma Guo <emmaguo100@gmail.com>
2022-04-06 20:34:12 -0700

    I added jobDetails pages to show the details of each job client clicks.

---
+++ client/src/components/jobs/jobDetails.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Jobs.css";
+
+function JobDetails({match}){
+    console.log();
+         return(
+            <section>
+                <div className="jobs-table">
+                    <div>
+                        <h1>Job details</h1>
+                            
+                    </div>
+                    
+                </div>
+
+                </section>
+         )
+
+}
+
+export default JobDetails;
