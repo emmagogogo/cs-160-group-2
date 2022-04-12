67ade7cd7af0ea578567726f9405a80e408f5289
Joshua Chin <zychin2000@gmail.com>
2022-04-11 20:24:09 -0700

    Added job application model

--- models/JobApplication.js
+++ models/JobApplication.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const JobApplicationSchema = new Schema({
-
     job: {
         id: {
             type: Schema.Types.ObjectId
@@ -17,13 +16,18 @@ const JobApplicationSchema = new Schema({
     user: {
         type: Schema.Types.ObjectId
     },
-
     stage: {
-        type: String
+        type: String // APPLIED, PHONE, FINAL, HIRED
     },
     date: {
         type: Date,
         default: Date.now
+    },
+    resume: {
+        type: String // Link to resume
+    },
+    coverLetter: {
+        type: String // Link to resume
     }
 });
 
