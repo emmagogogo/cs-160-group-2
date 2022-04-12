c27d465983d8f7108df6b4aa4040a5e24235f919
Joshua Chin <zychin2000@gmail.com>
2022-04-11 18:46:02 -0700

    added get jobs by id; delete job by id; added postedBy functionality

--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const Job = require("../../models/jobModel");
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
+const checkObjectId = require('../../middleware/checkObjectId');
 
 
 router.get("/getalljobs", async(req, res) => {
@@ -16,9 +19,12 @@ router.get("/getalljobs", async(req, res) => {
 
 });
 
-router.post("/postjob", async(req, res) => {
+router.post("/postjob", auth, async(req, res) => {
     try {
         const newJob = new Job(req.body);
+        // const user = await User.findById(req.user.id).select('-password');
+
+        newJob.postedBy = req.user.id;
         await newJob.save();
         res.send('Job Posted successfully');
     } catch (error) {
@@ -26,4 +32,50 @@ router.post("/postjob", async(req, res) => {
     }
 
 });
+
+// @route    GET api/jobs/:id
+// @desc     Get post by ID
+// @access   Private
+router.get('/:id', auth, checkObjectId('id'), async (req, res) => {
+    try {
+      const job = await Job.findById(req.params.id);
+  
+      if (!job) {
+        return res.status(404).json({ msg: 'Job not found' });
+      }
+  
+      res.json(job);
+    } catch (err) {
+      console.error(err.message);
+  
+      res.status(500).send('Server Error');
+    }
+  });
+
+// @route    DELETE api/posts/:id
+// @desc     Delete a post
+// @access   Private
+router.delete('/:id', [auth, checkObjectId('id')], async (req, res) => {
+    try {
+      const job = await Job.findById(req.params.id);
+  
+      if (!job) {
+        return res.status(404).json({ msg: 'Post not found' });
+      }
+  
+      // Check user
+      if (job.postedBy.toString() !== req.user.id) {
+        return res.status(401).json({ msg: 'User not authorized' });
+      }
+  
+      await job.remove();
+  
+      res.json({ msg: 'Post removed' });
+    } catch (err) {
+      console.error(err.message);
+  
+      res.status(500).send('Server Error');
+    }
+  });
+
 module.exports = router;
