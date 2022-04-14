54c21bf27688f76c83864148d051e570917360a8
Saif-El-Eslam <SAIF82820@GMAIL.COM>
2022-04-13 20:09:31 -0700

    Update index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,14 @@ import alert from './alert';
 import auth from './auth';
 import profile from './profile';
 import post from './post';
+import message from './message';
+import conversation from './conversation';
 
 export default combineReducers({
   alert,
   auth,
   profile,
-  post
+  post,
+  message,
+  conversation
 });
