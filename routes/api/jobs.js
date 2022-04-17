4c4f9cb4fa8c323e1c4a767fe3231a6ccca984da
Joshua Chin <zychin2000@gmail.com>
2022-04-17 16:48:41 -0700

    Added job search functionality for backend

--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -19,6 +19,26 @@ router.get("/getalljobs", async(req, res) => {
 
 });
 
+
+// @route    GET api/search?searchQuery=:searchQuery
+// @desc     Search for a job posting based on the query
+// @access   Public
+router.get("/search", async(req, res) => {
+  try{
+    const searchQuery = req.query.searchQuery
+
+    const jobs = await Job.find({'$text': {
+      '$search': searchQuery
+    }})
+
+    res.send(jobs)
+  }catch(error){
+    console.error(error.message);
+    return res.status(400).json({ error});
+
+}
+})
+
 router.post("/postjob", auth, async(req, res) => {
     try {
         const newJob = new Job(req.body);
