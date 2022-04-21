a631530f524284d4764f24f8f67e7dd36aaa991f
Emma Guo <emmaguo100@gmail.com>
2022-04-20 20:07:29 -0700

    modify register user api

--- routes/api/users.js
+++ routes/api/users.js
@@ -26,7 +26,7 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { name, email, password } = req.body;
+    const { name, email, password, userType } = req.body;
 
     try {
       let user = await User.findOne({ email });
@@ -50,7 +50,8 @@ router.post(
         name,
         email,
         avatar,
-        password
+        password,
+        userType
       });
 
       const salt = await bcrypt.genSalt(10);
