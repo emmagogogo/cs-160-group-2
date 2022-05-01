f19c14393d73e696c49cebc8dc7af752731c8579
Emma Guo <emmaguo100@gmail.com>
2022-04-30 20:51:48 -0700

    PostedJobs components

---
+++ client/src/components/postedJobs/PostedJobs.js
@@ -0,0 +1,7 @@
+//import React, {useState, useEffect} from 'react';
+import React from 'react';
+import './postedJob.css';
+import{useSelector} from 'react-redux';
+import{Table} from 'antd';
+import moment from 'moment';
+import {EditOutlined} from '@ant-design/icons'
