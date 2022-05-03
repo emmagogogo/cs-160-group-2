9c6f5e7822cc0dbb863655c83553aa9d378935a5
Joshua Chin <zychin2000@gmail.com>
2022-05-02 19:43:41 -0700

    added methods cors

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ connectDB();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
+  res.header("Access-Control-Allow-Methods", "*");
   next();
 });
 
